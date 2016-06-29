64da985e0006a6eea921c168e08ed9a569296a8e
SyuShengWei <feather8405@gmail.com>
2016-06-29 14:16:57 +0800

    用來讓每個月的資料按照日期分割 用法：1.將which_moon 改成要的月份(兩碼->ex:01)       2.將檔名改成該月份的csv(不用加上.csv)       3.將檔案路徑修改成自己電腦的路徑

--- by_date.py
+++ by_date.py
@@ -1,15 +1,17 @@
-inFile = open('c:/Users/SyuShengWei/Desktop/Youbike/20140101-31DIGES.csv','r',encoding ='UTF-8')
+which_moon = '00' 
+which_infile = 'fileNameWithoutCSV'
+inFile = open('c:/Users/SyuShengWei/Desktop/Youbike/Open data/'+which_infile+'.csv','r',encoding ='UTF-8')
 
 for x in range(1,10,1):
-	outFile = open ('c:/Users/SyuShengWei/Desktop/Youbike/by_date/01/2014-01-0' + str(x) +'.txt','a+')
+	outFile = open ('c:/Users/SyuShengWei/Desktop/Youbike/by_date/'+ which_moon +'/2014-' + which_moon + '-0' + str(x) +'.csv','a+')
 	outFile.write('扣款時間           ,借車時間,借車場站,還車時間,還車場站,租用(分) \n')
 	outFile.close()
 for x in range(10,32,1):
-	outFile = open ('c:/Users/SyuShengWei/Desktop/Youbike/by_date/01/2014-01-' + str(x) +'.txt','a+')
+	outFile = open ('c:/Users/SyuShengWei/Desktop/Youbike/by_date/' + which_moon + '/2014-' + which_moon + '-' + str(x) +'.csv','a+')
 	outFile.write('扣款時間           ,借車時間,借車場站,還車時間,還車場站,租用(分) \n')
 	outFile.close()
 	
-outFile = open ('c:/Users/SyuShengWei/Desktop/Youbike/by_date/01/uselessDeta.txt','a+')
+outFile = open ('c:/Users/SyuShengWei/Desktop/Youbike/by_date/' + which_moon + '/uselessDeta.csv','a+')
 outFile.write('扣款時間           ,借車時間,借車場站,還車時間,還車場站,租用(分) \n')
 outFile.close()
 
@@ -22,15 +24,15 @@ while True :
 	else :
 		judgement = list()
 		judgement = theLine.split(",")
-		if '2014-01-' not in judgement[1]:
-			outFile = open ('c:/Users/SyuShengWei/Desktop/Youbike/by_date/01/uselessDeta.txt','a+')
+		if '2014-' + which_moon + '-' not in judgement[1]:
+			outFile = open ('c:/Users/SyuShengWei/Desktop/Youbike/by_date/' + which_moon + '/uselessDeta.csv','a+')
 			outFile.write(theLine)
 			
 			ctr=ctr+1
 			print(ctr)
 		else:
 				whichFile = str(judgement[1]).split(' ')
-				outFile = open ('c:/Users/SyuShengWei/Desktop/Youbike/by_date/01/'+ whichFile[0] +'.txt','a+')
+				outFile = open ('c:/Users/SyuShengWei/Desktop/Youbike/by_date/' + which_moon + '/'+ whichFile[0] +'.csv','a+')
 				outFile.write(theLine)
 				
 				ctr=ctr+1
@@ -39,13 +41,13 @@ while True :
 
 
 for x in range(1,10,1):
-	outFile = open ('c:/Users/SyuShengWei/Desktop/Youbike/by_date/01/2014-01-0' + str(x) +'.txt','a+')
+	outFile = open ('c:/Users/SyuShengWei/Desktop/Youbike/by_date/' + which_moon + '/2014-' + which_moon + '-0' + str(x) +'.csv','a+')
 	outFile.close()
 for x in range(10,32,1):
-	outFile = open ('c:/Users/SyuShengWei/Desktop/Youbike/by_date/01/2014-01-' + str(x) +'.txt','a+')
+	outFile = open ('c:/Users/SyuShengWei/Desktop/Youbike/by_date/' + which_moon + '/2014-' + which_moon + '-' + str(x) +'.csv','a+')
 	outFile.close()
 	
-outFile = open ('c:/Users/SyuShengWei/Desktop/Youbike/by_date/01/uselessDeta.txt','a+')
+outFile = open ('c:/Users/SyuShengWei/Desktop/Youbike/by_date/' + which_moon + '/uselessDeta.csv','a+')
 outFile.close()
 						
 
